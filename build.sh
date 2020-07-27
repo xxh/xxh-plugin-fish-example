d276f364137e860022d4bda9d487a0704f692ea8
Frederick Henderson <frederickjh@henderson-meier.org>
2020-07-27 12:02:10 +0200

    Adds helpful example code that can be used by new plugin coders. Colors and copying of files with their required directory structure.

--- build.sh
+++ build.sh
@@ -13,6 +13,46 @@ do
   esac
 done
 
+function loadcolor(){
+# Colors  http://wiki.bash-hackers.org/snipplets/add_color_to_your_scripts
+# More info about colors in bash http://misc.flogisoft.com/bash/tip_colors_and_formatting
+esc_seq="\x1b["  #In Bash, the <Esc> character can be obtained with the following syntaxes:  \e  \033  \x1B
+NC=$esc_seq"39;49;00m" # NC = Normal Color
+# Colors with black background (40;)set for emails.
+red=$esc_seq"31;40;01m"
+green=$esc_seq"32;40;00m"
+yellow=$esc_seq"33;40;01m"
+blue=$esc_seq"34;40;01m"
+magenta=$esc_seq"35;40;01m"
+cyan=$esc_seq"36;40;01m"
+}
+loadcolor
+
+# Makes a directory if it is not found.
+function mkdirifnotfound(){
+  mkdir -p "$build_dir/$founddirectory"
+}
+
+###### Some example code to help jump start new fish plugins.
+###### This code does the not so easy task of copying files with their unknown directory structure.
+
+### Copy files to the build directory that will be uploaded to remote hosts.
+### This defaults to the "pluginrc.fish" file but other can be added after it separated by a space.
+#for filestocopy in pluginrc.fish
+#do
+#  for found in $(find "$filestocopy" -depth -print)
+#  do
+#    if [ -f "$found" ]; then
+#      founddirectory=$(dirname "$found")
+#      mkdirifnotfound
+#      cp -r "$found" "$build_dir"
+#    else
+#      mkdirifnotfound
+#    fi
+#  done
+#done
+
+
 rm -rf $build_dir
 mkdir -p $build_dir
 
--- pluginrc.fish
+++ pluginrc.fish
@@ -1,3 +1,42 @@
+#!/usr/bin/env fish
+# Next line for testing.
+#set fish_trace true
+
+set_color blue
+
+# Set the location of this plugin's build directory on the remote host.
+set CURRENT_DIR (dirname (realpath (status current-filename)))
+
+cd $CURRENT_DIR
+
+###### Some example code to help jump start new fish plugins.
+###### This code does the not so easy task of copying files with their unknown directory structure.
+
+### Set the base directory that we want directories and files copied into on the remote server.
+### Normally this will be into $XDG_CONFIG_HOME which is set by default by xxh to:
+### $HOME/.xxh/.config
+#set basedirectorytocopyfilesto $XDG_CONFIG_HOME
+
+### Makes a directory if it is not found.
+#function mkdirifnotfound
+#    mkdir -p "$basedirectorytocopyfilesto/$founddirectory"
+#end
+
+### Set files then copy them. By default is this the files in the "fish" directory,
+### that would normally be found in ~/.config/fish
+#set filestocopy "fish"
+#for found in (find "$filestocopy" -depth -print)
+#    if test -f "$found"
+#      set founddirectory (dirname "$found")
+#      mkdirifnotfound
+#      cp "$found" "$basedirectorytocopyfilesto/$founddirectory/"
+#    else
+#      mkdirifnotfound
+#    end
+#end
+
 set_color yellow; echo -n ">>> "
 set_color red; echo "xxh-plugin-fish-example is initialized!"
 set_color normal
+# For testing, ends trace.
+#set -e fish_trace
